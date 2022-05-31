c31ae6307365616839fe2d155124a1612c812c25
趙子賢 <danny900714@gmail.com>
2022-06-01 03:55:37 +0800

    Add push main build discover

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/dockcenter/paper
+
+go 1.18
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	golang.org/x/mod v0.5.1
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+)
---
+++ internal/app/discover/promotion.go
@@ -0,0 +1,41 @@
+package discover
+
+import (
+	"fmt"
+	"strings"
+
+	"golang.org/x/mod/semver"
+)
+
+type Promotion struct {
+	Version     string
+	Build       int
+	DownloadURL string
+	Latest      bool
+	Major       bool
+	MajorMinor  bool
+}
+
+func (p Promotion) Semver() string {
+	return fmt.Sprintf("%s+%d", semver.Canonical("v"+p.Version), p.Build)
+}
+
+func (p Promotion) DockerTags() string {
+	var tags []string
+	tags = append(tags, strings.ReplaceAll(p.Semver()[1:], "+", "-r"))
+	tags = append(tags, semver.Canonical(p.Semver())[1:])
+
+	if p.MajorMinor {
+		tags = append(tags, semver.MajorMinor(p.Semver())[1:])
+	}
+
+	if p.Major {
+		tags = append(tags, semver.Major(p.Semver())[1:])
+	}
+
+	if p.Latest {
+		tags = append(tags, "latest")
+	}
+
+	return strings.Join(tags, ",")
+}
