47cfbf4707537e4e769afc0e28d1bbd8466833b6
趙子賢 <danny900714@gmail.com>
2022-06-02 03:02:21 +0800

    Change BASE_IMAGE_TAG according to paper version

--- internal/app/discover/command.go
+++ internal/app/discover/command.go
@@ -9,5 +9,14 @@ func BuildCommand(promotion Promotion, environment string) string {
 	if environment == "" {
 		environment = semver.Canonical(promotion.Semver())[1:]
 	}
-	return fmt.Sprintf("drone build promote \"$DRONE_REPO\" \"$DRONE_BUILD_NUMBER\" %s --param=DOWNLOAD_URL=%s --param=DOCKER_TAGS=%s", environment, promotion.DownloadURL, promotion.DockerTags())
+
+	// Calculate BASE_IMAGE_TAG
+	var baseImageTag string
+	if semver.Compare(promotion.Semver(), "v1.17.1") < 0 {
+		baseImageTag = "16-jdk-alpine"
+	} else {
+		baseImageTag = "17-jre-alpine"
+	}
+
+	return fmt.Sprintf("drone build promote \"$DRONE_REPO\" \"$DRONE_BUILD_NUMBER\" %s --param=BASE_IMAGE_TAG=%s --param=DOWNLOAD_URL=%s --param=DOCKER_TAGS=%s", environment, baseImageTag, promotion.DownloadURL, promotion.DockerTags())
 }
